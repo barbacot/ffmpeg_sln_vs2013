d0c9d70f8a4ac06562e555224b47b5890edb1045
unknown <fpirvan@RO-BUC-TESTPC01.VRR.local>
2015-03-13 16:11:35 +0200

    First commit

---
+++ compat/msvcrt/snprintf.d
@@ -0,0 +1,2 @@
+libavutil/../compat/msvcrt/snprintf.o: F:/extern/ffmpeg-vs2013/compat/va_copy.h
+libavutil/../compat/msvcrt/snprintf.o: F:/extern/ffmpeg-vs2013/libavutil/error.h
---
+++ compat/strtod.d
@@ -0,0 +1,5 @@
+libavutil/../compat/strtod.o: F:/extern/ffmpeg-vs2013/libavutil/avstring.h
+libavutil/../compat/strtod.o: f:/extern/ffmpeg-vs2013/libavutil/attributes.h
+libavutil/../compat/strtod.o: F:/extern/ffmpeg-vs2013/libavutil/mathematics.h
+libavutil/../compat/strtod.o: f:/extern/ffmpeg-vs2013/libavutil/rational.h
+libavutil/../compat/strtod.o: f:/extern/ffmpeg-vs2013/libavutil/intfloat.h
